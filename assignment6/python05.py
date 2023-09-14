0847fbe6efdb4fb5fd5d1a0122222cd1bd2519cb
c2-80462 <rajatkakane14@gmail.com>
2023-09-14 23:29:25 +0530

    day 3 assignment

--- assignment6/python05.py
+++ assignment6/python05.py
@@ -3,12 +3,16 @@
 
 def calc():
     sum=0
+    count=0
     Str1="Python83737@#8496"
     for nums in Str1:
         if nums.isdigit() == True:
             nums = int(nums)
             sum += nums
+            count += 1
+    avg = sum/count
     print(f"Total sum is {sum}")
+    print(f"Average is {avg}")
 
 
 calc()
